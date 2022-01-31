db746dad8a0a82bad4ed88774216dc89361ea6af
Laryssa Souza Ramos <laryssaramos284@gmail.com>
2022-01-31 17:37:54 -0300

    .

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hora_do_dia
+
+<p aling="center">
+    <img src="hora_do_dia/assets/manha.png">
+</p>
  
---
+++ script.js
@@ -0,0 +1,30 @@
+function carregar(){
+
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora =  data.getHours()
+    var min = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${min}`
+    
+    if (hora >= 0 && hora < 12){
+        img.src = 'img/manha.jpg'
+        document.body.style.background = '#aba1ab'
+        saudacao.innerHTML = 'Bom dia!'
+
+    } else if (hora >= 12 && hora < 19){
+        img.src = 'img/tarde.jpg'
+        document.body.style.background = '#aa725d'
+        saudacao.innerHTML = 'Boa Tarde!'
+        
+    } else{
+        img.src = 'img/noite.jpg'
+        document.body.style.background = '#1d393d'
+        saudacao.innerHTML = 'Boa Noite!'
+
+    }
+    
+}
+
+
+
